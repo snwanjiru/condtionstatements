652ed314d09eccdc8874e6e3ca38e93fafa66a98
Wanjiru <snwanjiru03@gamil.com>
2024-02-21 20:21:25 +0300

    This is my first commit

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿//string permission = "Admin|Manager";
+
+Console.WriteLine("Enter username:");
+string userName = Console.ReadLine();
+
+
+Console.WriteLine("Enter level:");
+int level = Convert.ToInt32(Console.ReadLine());
+
+
+if (userName.Contains("Admin"))
+{
+    if (level > 55)
+    {
+        Console.WriteLine("Welcome, Super Admin user.");
+    }
+    else
+    {
+        Console.WriteLine("Welcome, Admin user.");
+    }
+}
+else if (userName.Contains("Manager"))
+{
+    if (level >= 20)
+    {
+        Console.WriteLine("Contact an Admin for access.");
+    }
+    else
+    {
+        Console.WriteLine("You do not have sufficient privileges.");
+    }
+}
+else
+{
+    Console.WriteLine("You do not have sufficient privileges.");
+}
